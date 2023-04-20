e2a538307374fb1db54b87a8576bd0fa6418f032
Belinda <Belzsena@gmail.com>
2023-04-20 15:03:50 -0700

    Task 5 Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -Program that print a line using printf function
+ * main - Program that print a line using printf function
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
 }
