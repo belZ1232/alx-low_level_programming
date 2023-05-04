5841a9e414835251cdc08d6c8280d0fe447ec2f3
Belinda <Belzsena@gmail.com>
2023-05-04 13:53:54 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -15,7 +15,9 @@ p = &n;
 * - am not allowed to modify p
 * - am not allowed to code anything else than this line of code.
 */
+
 *(p + 2) = 98;
+std::cout << "a[2] = " << *(p + 2) << std::endl;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
