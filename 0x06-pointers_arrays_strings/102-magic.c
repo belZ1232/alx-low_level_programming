e52ead8f377dd5888fefb3a131f083dcd3aeaa8a
Belinda <Belzsena@gmail.com>
2023-05-04 14:50:40 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,7 +3,7 @@
 int _putchar(char);
 {
 int n;
-int a[2];
+int a[5];
 int *p;
 
 a[2] = 1024;
@@ -15,9 +15,7 @@ p = &n;
 * - am not allowed to modify p
 * - am not allowed to code anything else than this line of code.
 */
-
-*(p + 2) = 98
-std::cout << "a[2] = " << *(p + 2) << std::endl;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
