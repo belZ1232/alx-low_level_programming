e2735cf177a0c2f79e6705fd137818c7892a7f01
Belinda <Belzsena@gmail.com>
2023-05-16 04:35:30 -0700

    Task 0 creates an array of chars and initialize

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
 * create_array - write the array of chars
-* and initializes it with a specific char.
-* @c: char to initialize
+* and initialized specific char.
+* @c: char to be initialized
 * @size: The number of bytes to be  allocated
-*  Return: a pointer to the array or null.
+* Return: a pointer to the array or null.
 */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 char *array = malloc(size);
+
 if (size == 0 || array == 0)
-{
 return (NULL);
-}
-while (unsigned int i = 0, i < size; i++)
-{
-array[i] = c;
-}
+
+while (size--)
+array[size] = c;
 return (array);
+
 }
+
