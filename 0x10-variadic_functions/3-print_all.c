2dfa3efb82fc224de7baab2805956b2400e7997b
Belinda <Belzsena@gmail.com>
2023-05-25 04:50:19 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,8 +2,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
-* print_all - funct dat prints anything
-* @format: the types of arguments passed to the func
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
 */
 void print_all(const char * const format, ...)
 {
