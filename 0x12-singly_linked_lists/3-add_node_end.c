197fa062c575a659aee0e22a3c6fe3c43996f91e
belZ1232 <Belzsena@gmail.com>
2023-06-01 14:33:08 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,27 +3,27 @@
 #include "lists.h"
 
 /**
-* add_node_end - func dat adds a new node
+* add_node_end - new node to be added
 * at the end of a linked list
 * @head: a double pointer to the list_t list
 * @str: put a string in the new node
-* Return: an address of the new element, or NULL if it failed
+* Return: address of the new element, or NULL if it failed
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new;
 list_t *temp = *head;
-unsigned int length = 0;
+unsigned int len = 0;
 
-while (str[length])
-length++;
+while (str[len])
+len++;
 
 new = malloc(sizeof(list_t));
 if (!new)
 return (NULL);
 
 new->str = strdup(str);
-new->length = length;
+new->len = len;
 new->next = NULL;
 
 if (*head == NULL)
@@ -39,3 +39,4 @@ temp->next = new;
 
 return (new);
 }
+
