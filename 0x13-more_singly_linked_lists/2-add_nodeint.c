cbe740a43e27e3bb0693cfe910424c4aae2f9282
belZ1232 <Belzsena@gmail.com>
2023-06-06 14:27:34 -0700

    Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* add_nodeint - func dat add a new node at the beginning of list
+* @head: it is a pointer to a pointer of head list
+* @n: it is the valued to be added
+* Return: element address or Null if failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t new_node = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->n = *head;
+	*head = new_node;
+	return (new_node);
+}
