acf325151270840c63a9527e733c89b8f3852efd
belZ1232 <Belzsena@gmail.com>
2023-06-06 15:09:42 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->n = *head;
+	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
