4a3ea79afcb5878f39290afb5baafe2133ad5f1b
belZ1232 <Belzsena@gmail.com>
2023-06-07 15:01:43 -0700

    Task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - func dart insert new node in list
+* @head: it is a pointer of first node
+* @idx: it is an index that new nodes added
+* @n: data that inserted in new  nodes
+* Return: pointer or NULL
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *temp_node = *head;
+	unsigned int b;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node || !head)
+		return (NULL);
+	new_node->n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	for (b = 0; temp_node && b < idx; b++)
+	{
+		if (b == idx - 1)
+		{
+			new_node->next = temp_node->next;
+			temp_node->next = new_node;
+			return (new_node);
+		}
+		else
+		temp_node = temp_node->next;
+	}
+	return (NULL);
+}
