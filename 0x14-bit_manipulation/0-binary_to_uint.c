24a7e4816163f3e25e655e62f322054a6851af37
belZ1232 <Belzsena@gmail.com>
2023-08-09 01:11:25 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,37 @@
 #include "main.h"
+
 /**
-* binary_to_unity - change binary to unsigned int.
-* @b: binary
-* of 0 and 1 char
-* Return: 0 if thers is 1 or more characters
-* in the string
-*/
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int res = 0;
-if (b == NULL)
-return (0);
-for (int p = 0; b[p] != '\0'; p++)
-{
-if (b[p] == '0')
-{
-res = res << 1;
+	unsigned int ci;
+	int bee, bas;
+
+	if (!b)
+		return (0);
+
+	ci = 0;
+
+	for (bee = 0; b[bee] != '\0'; bee++)
+		;
+
+	for (bee--, bas = 1; bee >= 0; bee--, bas *= 2)
+	{
+		if (b[bee] != '0' && b[bee] != '1')
+		{
+			return (0);
+		}
+
+		if (b[bee] & 1)
+		{
+			ci += bas;
+		}
+	}
+
+	return (ci);
 }
-else if (b[p] == '1')
-{
-res = (res << 1) | 1;
-}
-else
-{
-return (0);
-}
-}
-return (res);
-}
-/**
- * main - Entry point
- * Return: 0 (successful)
-int main(void)
-{
-const char *binary = "101000";
-unsigned int decimal = binary_to_uint(binary);
-printf("Binary: %s\nDecimal: %u\n", binary, decimal);
-return (0);
-}
+
