8fe035469d58ca28ed4bdd61e2f29411103da381
belZ1232 <Belzsena@gmail.com>
2023-08-09 12:28:16 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,57 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file
- * and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
- */
+* read_textfile - reads a text file
+* and prints the letters
+* @filename: filename.
+* @letters: numbers of letters printed.
+*
+* Return: numbers of letters printed. It fails, returns 0.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int ax;
-	ssize_t bee, pee;
-	char *buf;
+int ax;
+ssize_t bee, pee;
+char *buf;
 
-	if (!filename)
-		return (0);
+if (!filename)
+return (0);
 
-	ax = open(filename, O_RDONLY);
+ax = open(filename, O_RDONLY);
 
-	if (ax == -1)
-		return (0);
+if (ax == -1)
+return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
+return (0);
 
-	bee = read(ax, buf, letters);
-	pee = write(STDOUT_FILENO, buf, bee);
+bee = read(ax, buf, letters);
+pee = write(STDOUT_FILENO, buf, bee);
 
-	close(ax);
+close(ax);
 
-	free(buf);
+free(buf);
 
-	return (pee);
+return (pee);
+}
+/**
+* main - Entry point
+* Return: 0(successful)
+*/
+int main(void)
+{
+const char *filename = "example.txt";
+size_t letters = 110;
+
+ssize_t result = read_textfile(filename, letters);
+if (result == 0)
+{
+printf("Failed to read the file.\n");
+return (1);
 }
 
+printf("Successfully read and printed %zd letters from the file.\n", result);
+return (0);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
