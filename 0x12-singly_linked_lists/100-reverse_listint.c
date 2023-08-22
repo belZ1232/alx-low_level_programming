be9476b96aae1d80241be4ed774669fbd7ea0736
belZ1232 <Belzsena@gmail.com>
2023-08-22 06:36:50 -0700

    Reverses linked list

--- 0x12-singly_linked_lists/100-reverse_listint.c
+++ 0x12-singly_linked_lists/100-reverse_listint.c
@@ -7,20 +7,20 @@
  */
 
 listint_t *reverse_listint(listint_t **head)
-
 {
-	listint_t *p = NULL;
-	listint_t *n = NULL;
+ listint_t *p = NULL;
+ listint_t *n = NULL;
 
-	while (*head)
-	{
-		n = (*head)->n;
-		(*head)->n = p;
-		 p = *head;
-		 *head = n;
-	}
+ while (*head)
+ {
+ n = (*head)->n;
+ (*head)->n = p;
+ p = *head;
+ *head = n;
+ }
 
-	*head = p;
+ *head = p;
 
-	return (*head);
+ return (*head);
 }
+
